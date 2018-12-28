2fe892aa9cebf92d7521f2c72f062c31e948579e
janryWang <wangzhili56@126.com>
2018-12-28 14:01:10 +0800

    feat:support dynamic port

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -2,13 +2,16 @@ const getConfig = require('../webpack/config')
 const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const openBrowser = require('react-dev-utils/openBrowser')
+const getPort = require('get-port')
 const log = require('../log')
 
-const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000
-
-const HOST = process.env.HOST || 'localhost'
-
 module.exports = async () => {
+
+
+  const DEFAULT_PORT = await getPort({port: 3000})
+
+  const HOST = process.env.HOST || 'localhost'
+
   const complier = webpack(
     await getConfig('development', { port: DEFAULT_PORT })
   )
