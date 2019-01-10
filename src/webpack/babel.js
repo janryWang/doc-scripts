406e93f18fb880c9226531f42b332d159bcffe2b
janryWang <wangzhili56@126.com>
2019-01-10 11:26:16 +0800

    feat:support babel runtime

--- src/webpack/babel.js
+++ src/webpack/babel.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     // https://github.com/babel/babel/issues/8562
     [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+    require.resolve('@babel/plugin-transform-runtime'),
     require.resolve('@babel/plugin-proposal-class-properties'),
     require.resolve('@babel/plugin-syntax-dynamic-import'),
     require.resolve('babel-plugin-react-docgen')
