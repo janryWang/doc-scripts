8c659799a5d48a80a1f2ac5a7ca039687190761a
janryWang <wangzhili56@126.com>
2019-01-10 11:35:53 +0800

    feat:support catch error in node api

--- src/index.js
+++ src/index.js
@@ -5,20 +5,16 @@ import chalk from "chalk"
 import log from "./log"
 
 export const execute = async (cmd, options, webpackConfig) => {
+    const script = `./scripts/${cmd}.js`
     try {
-        const script = `./scripts/${cmd}.js`
         await fs.access(path.resolve(__dirname, script))
-        try {
-            await require(script)(options, webpackConfig)
-        } catch (e) {
-            throw e && (e.stack || e.message)
-                ? e.stack
-                    ? e.stack
-                    : e.message
-                : e
-        }
     } catch (e) {
         throw chalk.red("Executed a command that does not exist.")
+    }
+    try {
+        await require(script)(options, webpackConfig)
+    } catch (e) {
+        throw e && (e.stack || e.message) ? (e.stack ? e.stack : e.message) : e
     }
 }
 
