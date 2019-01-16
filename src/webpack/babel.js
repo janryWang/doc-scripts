51402f988a2c7ec70c993779e2202562b2f09dba
janryWang <wangzhili56@126.com>
2019-01-17 00:03:20 +0800

    feat:add babel-plugin-add-module-exports

--- src/webpack/babel.js
+++ src/webpack/babel.js
@@ -3,6 +3,7 @@ module.exports = {
     // https://github.com/babel/babel/issues/8562
     [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
     require.resolve('@babel/plugin-transform-runtime'),
+    require.resolve('babel-plugin-add-module-exports'),
     require.resolve('@babel/plugin-proposal-class-properties'),
     require.resolve('@babel/plugin-syntax-dynamic-import'),
     require.resolve('babel-plugin-react-docgen')
